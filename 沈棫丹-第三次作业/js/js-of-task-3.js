66b779fc81a77bb0265799ce0938e6bed3225542
Shenyudan <1312764234@qq.com>
2017-08-31 18:47:06 +0800

    沈棫丹第三次作业

---
+++ 沈棫丹-第三次作业/js/js-of-task-3.js
@@ -0,0 +1,57 @@
+$(document).ready(function(){
+  //点击事件明显是个重复事件，但是我还不知道怎么把这五个点击合并，希望下次去工作室的时候学长能讲一下
+  $(".first").click(function(){
+    $("#tab-content").html("");
+    $.ajax({  
+    url:"http://rapapi.org/mockjsdata/14169/geek",  
+    dataType: "json",  
+    success: function(responseText){  
+    var i=responseText.list.length; 
+      $("#tab-content").html(responseText.list[i-5])}  
+    });
+  });
+
+  $(".second").click(function(){
+    $("#tab-content").html("");
+    $.ajax({  
+    url:"http://rapapi.org/mockjsdata/14169/geek",  
+    dataType: "json",  
+    success: function(responseText){  
+    var i=responseText.list.length; 
+      $("#tab-content").html(responseText.list[i-4])}
+    });
+  });
+
+  $(".third").click(function(){
+    $("#tab-content").html("");
+    $.ajax({  
+    url:"http://rapapi.org/mockjsdata/14169/geek",  
+    dataType: "json",  
+    success: function(responseText){  
+    var i=responseText.list.length; 
+      $("#tab-content").html(responseText.list[i-3])} 
+    });
+  });
+
+  $(".forth").click(function(){
+    $("#tab-content").html("");
+    $.ajax({  
+    url:"http://rapapi.org/mockjsdata/14169/geek",  
+    dataType: "json",  
+    success: function(responseText){  
+    var i=responseText.list.length; 
+      $("#tab-content").html(responseText.list[i-2])} 
+    });
+  });
+
+  $(".fifth").click(function(){
+    $("#tab-content").html("");
+    $.ajax({  
+    url:"http://rapapi.org/mockjsdata/14169/geek",  
+    dataType: "json",  
+    success: function(responseText){  
+    var i=responseText.list.length; 
+      $("#tab-content").html(responseText.list[i-1])}  
+    });
+  });
+});
